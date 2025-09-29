d272754e249fbfd34b01ac467cf514a61b798766
rayyansurani <rayyan.surani@hotmail.com>
2025-09-29 13:07:26 -0400

    Initial Commit

--- utils.js
+++ utils.js
@@ -1,40 +1,51 @@
-import { verifyKey } from 'discord-interactions';
+import 'dotenv/config';
 
-export function VerifyDiscordRequest(clientKey) {
-  return function (req, res, buf, encoding) {
-    const signature = req.get('X-Signature-Ed25519');
-    const timestamp = req.get('X-Signature-Timestamp');
-
-    const isValidRequest = verifyKey(buf, signature, timestamp, clientKey);
-    if (!isValidRequest) {
-      return res.status(401).end('Bad request signature');
+export async function DiscordRequest(endpoint, options) {
+    // append endpoint to root API URL
+    const url = 'https://discord.com/api/v10/' + endpoint;
+    // Stringify payloads
+    if (options.body) options.body = JSON.stringify(options.body);
+    // Use fetch to make requests
+    const res = await fetch(url, {
+        headers: {
+            Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+            'Content-Type': 'application/json; charset=UTF-8',
+            'User-Agent': 'DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)',
+        },
+        ...options
+    });
+    // throw API errors
+    if (!res.ok) {
+        const data = await res.json();
+        console.log(res.status);
+        throw new Error(JSON.stringify(data));
     }
-  }
+    // return original response
+    return res;
 }
 
-export function DiscordAPI(url) { return 'https://discord.com/api/v9/' + url };
+export async function InstallGlobalCommands(appId, commands) {
+    // API endpoint to overwrite global commands
+    const endpoint = `applications/${appId}/commands`;
+
+    try {
+        // This is calling the bulk overwrite endpoint: https://discord.com/developers/docs/interactions/application-commands#bulk-overwrite-global-application-commands
+        await DiscordRequest(endpoint, { method: 'PUT', body: commands });
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 // Simple method that returns a random emoji from list
 export function getRandomEmoji() {
-  let emojiList = ['😭', '😄', '😌', '🤓', '😎', '😤', '🤖', '😶‍🌫️', '🌏', '📸', '💿', '👋', '🌊', '✨'];
-  return emojiList[Math.floor(Math.random() * emojiList.length)];
+    const emojiList = ['😭','😄','😌','🤓','😎','😤','🤖','😶‍🌫️','🌏','📸','💿','👋','🌊','✨'];
+    return emojiList[Math.floor(Math.random() * emojiList.length)];
+}
+
+export function getHeadTail(strArr) {
+    return strArr[Math.floor(Math.random() * strArr.length)];
 }
 
 export function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-export const ComponentType = {
-    ACTION: 1,
-    BUTTON: 2,
-    SELECT: 3,
-    INPUT: 4
-}
-
-export const ButtonStyle = {
-    PRIMARY: 1,
-    SECONDARY: 2,
-    SUCCESS: 3,
-    DANGER: 4,
-    LINK: 5
 }
